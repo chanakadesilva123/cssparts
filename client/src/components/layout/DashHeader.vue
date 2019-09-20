4bb27abf14f384777dd989c4918d9b95ef5f6b46
Chanaka <chanaka@de-silva.com.au>
2019-09-21 01:38:59 +0800

    Initialise client

---
+++ client/src/components/layout/DashHeader.vue
@@ -0,0 +1,20 @@
+<template>
+ <v-app-bar app color="black" style="color:white">
+      <v-toolbar-title class="headline text-uppercase">
+        <v-img src="@/assets/logo.png" max-width="300" max-height="50"></v-img>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+      style="color:white"
+        text
+        href="#"
+        target="_top"
+      >
+        <span class="mr-2">Refresh</span>
+      </v-btn>
+    </v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
